f486dfe0c5235ade6dfcf7b84a2486019f9db430
Dillon John <dillon.a.john@gmail.com>
2024-09-20 22:27:57 -0400

    refactor: header-nav component
    refactor header-nav component to use conditional and loop directives
    to render the appropriate links based on the user's authentication status
    alongside the general navigation links

--- src/app/header-nav/header-nav.component.ts
+++ src/app/header-nav/header-nav.component.ts
@@ -5,8 +5,22 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header-nav.component.html',
-  styleUrl: './header-nav.component.css'
+  styleUrl: './header-nav.component.css',
 })
 export class HeaderNavComponent {
+  isAuthenticated = false;
 
+  navLinks = [
+    { url: '/', label: 'Home' },
+    { url: '/about', label: 'About' },
+    { url: '/services', label: 'Services' },
+    { url: '/contact', label: 'Contact' },
+  ];
+
+  navGuestLinks = [
+    { url: '/login', label: 'Sign In' },
+    { url: '/register', label: 'Register' },
+  ];
+
+  navAuthenticatedLinks = [{ url: '/profile', label: 'Profile' }];
 }
