3c45b2d1c08cad8ea1093cdbef7ae264d11e17da
Dillon John <dillon.a.john@gmail.com>
2024-09-21 11:07:28 -0400

    refactor: navigation
    add HomeComponent and LoginComponent to routes
    update header-nav component to use routerLink instead of href
    add router module to header-nav component
    remove leading slash from navLinks and navGuestLinks urls

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  // { path: '/about', component: AboutComponent },
+  // { path: '/services', component: ServicesComponent },
+  // { path: '/contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  // { path: '/register', component: RegisterComponent },
+];
