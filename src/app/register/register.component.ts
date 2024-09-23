4948ae85456f93dd4ecf24e387f52f10926cccd7
Dillon John <dillon.a.john@gmail.com>
2024-09-22 21:48:37 -0400

    feat: register-form component
    add register-form component for user registration
    update register component to include register-form component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
+import { RegisterFormComponent } from '../register-form/register-form.component';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RegisterFormComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
   CardComponent = CardComponent;
+  RegisterFormComponent = RegisterFormComponent;
 }
